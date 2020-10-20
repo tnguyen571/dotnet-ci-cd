f5b319dd59b4219bdcf7f96ace875eda370fee81
tnguyen571 <tnguyen571@dxc.com>
2020-10-20 14:37:29 +0700

    fix add user from function app http

--- ApiJWT/Models/User.cs
+++ ApiJWT/Models/User.cs
@@ -12,8 +12,6 @@ namespace ApiJWT.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Username { get; set; }
-
-        [JsonIgnore]
         public string Password { get; set; }
     }
 }
